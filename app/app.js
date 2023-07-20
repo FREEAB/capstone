f1e97a49f674917b768185f6883629d47f7e8667
Youssef Bamieh <youssefbamieh@MacBook-Air-2.local>
2023-07-20 09:03:53 -0700

    Created functional login page with post method

--- app/app.js
+++ app/app.js
@@ -8,9 +8,7 @@ app.set('view engine', 'ejs');
 
 //Setting middleware
 app.use(express.static(__dirname + '/public')); //This allows express to look for static (CSS, JS, Images, etc.) files in the /public folder
-
-
-
+app.set('views', __dirname + '/views'); //This allows express to look for views in the /views folder
 
 //Defining first route for incoming requests with no path in the URL
 app.get("/", async (req, res) => {
