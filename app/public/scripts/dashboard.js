d8dbdd55e580ec6edb46eb706da979b02a2a45a5
Youssef Bamieh <youssefbamieh@MacBook-Air-2.local>
2023-07-31 13:20:51 -0700

    Added documentation for dashboard.js

--- app/public/scripts/dashboard.js
+++ app/public/scripts/dashboard.js
@@ -1,13 +1,20 @@
-// const monthNames = ["January", "February", "March", "April", "May", "June",
-//   "July", "August", "September", "October", "November", "December"
-// ];
+/* Start of Bamieh's Code (Lines were edited by Gronemeier for front-end compatibility) */
 
+/**
+ * @param {Date} date - Date object for date that you want to add from
+ * @param {*} days - Amount of days you'd like to add to date
+ * @returns - Returns a date object with added days
+ */
 function addDays(date, days) {
     const dateCopy = new Date(date);
     dateCopy.setDate(date.getDate() + days);
     return dateCopy;
 }
 
+/**
+ * @param {Date} date - Date object you'd like to print
+ * @returns - String representation of date object
+ */
 function getFullDate(date) {
     let dayName = date.toLocaleString('default', { weekday: 'long' });
     let month = date.toLocaleString('default', { month: 'long' });
@@ -15,30 +22,29 @@ function getFullDate(date) {
     return (String(dayName) + " " + String(day) + " " + String(month));
 }
 
-// for (i = 0; i < 14; i++) {
-//     let dateElement = document.querySelector("#day_" + String(i) + "_header");
-//     const dateObj = new Date();
-//     dateElement.innerHTML = getFullDate(addDays(dateObj, i - 1));
-// }
-
+// Defining necessary variables
 const dateObj = new Date();
-
 const dayOfWeek = dateObj.getDay();
-
 const offset = 0 - dayOfWeek;
 
-
+// Loop through all day headers
 for (i = 0; i < 14; i++) {
+
+    // Grab all date_header elements and populate them with proper dates
     let dateElement = document.querySelector("#day_" + String(i) + "_header");
     let newDate = addDays(dateObj, i + offset);
-    let newDateDay = newDate.getDay();
     dateElement.innerHTML = getFullDate(newDate);
 
+    /* Figure out what day the header is on and if it is a 0 (Sunday) or 6 (Saturday) 
+    then grey out all corresponding boxes */
+    let newDateDay = newDate.getDay();
     if (newDateDay == 0 || newDateDay == 6) {
         let weekendElements = document.querySelectorAll(".day_" + String(i));
         weekendElements.forEach(element => {
-            element.setAttribute("disabled", true);
-            element.style.backgroundColor = "#d7d7d7";
+            element.setAttribute("disabled", true); // Disable button
+            element.style.backgroundColor = "#d7d7d7"; // Set it to grey
         })
     }
 }
+
+/* End of Bamieh's Code */
